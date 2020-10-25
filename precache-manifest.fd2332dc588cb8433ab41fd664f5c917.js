c590a21aebf060e4a9f9c4a4b53d8b50df05a76e
milanjacimovic <jacimovicmilanfon@gmail.com>
2020-10-25 23:40:05 +0100

    Updates

---
+++ precache-manifest.fd2332dc588cb8433ab41fd664f5c917.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "197a20e5a31f54189f83efefd17b98cb",
+    "url": "/Monsters-practice/index.html"
+  },
+  {
+    "revision": "fd3ad2614d6a12950938",
+    "url": "/Monsters-practice/static/css/main.74169b8d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Monsters-practice/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-practice/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd3ad2614d6a12950938",
+    "url": "/Monsters-practice/static/js/main.14cd4903.chunk.js"
+  },
+  {
+    "revision": "46dfbce59b3fe7b5a28d",
+    "url": "/Monsters-practice/static/js/runtime-main.fafe97cf.js"
+  }
+]);
